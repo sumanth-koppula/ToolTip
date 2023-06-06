cd0d8e69373a2ffd9a3d00927e934019154cad80
Sumanth <sumanth.koppula05@gmail.com>
2023-06-06 15:28:32 +0530

    ToolTip

---
+++ src/ToolTip.js
@@ -0,0 +1,33 @@
+import { useEffect, useRef } from "react";
+import './style/ToolTip.css'
+
+const ToolTip = ({position}) => {
+    const spanRef = useRef();
+    useEffect(()=>{
+        if(position==='top'){
+            spanRef.current.style.bottom = '120%';
+            spanRef.current.style.left = '50%';
+            spanRef.current.style.marginLeft = '-110px';
+        }
+        if (position === 'bottom') {
+            spanRef.current.style.top = '140%';
+            spanRef.current.style.left = '50%';
+            spanRef.current.style.marginLeft = '-110px';
+        }
+        if (position === 'left') {
+            spanRef.current.style.top = '-50%';
+            spanRef.current.style.right = '110%';
+        }
+        if (position === 'right') {
+            spanRef.current.style.top = '-50%';
+            spanRef.current.style.left = '110%';
+        }
+    },[position])
+    return (
+
+        <div className="tooltip">Hover over me!
+            <span ref={spanRef} className="tooltiptext">Thanks for hovering! I'm a tooltip</span>
+        </div>
+    )
+}
+export default ToolTip;
